8a943258c0947ac5f0d9fb11cd3ab99e3d700cda
abukawsar47 <abukawsar47ak@gmail.com>
2022-05-15 03:16:35 +0600

    module 73-74

--- src/Pages/Home/ExceptionalDental .js
+++ src/Pages/Home/ExceptionalDental .js
@@ -6,7 +6,7 @@ const ExceptionalDental = () => {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row">
-                <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" />
+                <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
